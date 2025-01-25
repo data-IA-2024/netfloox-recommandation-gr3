01d7c559d19402a6516cf0d34dabe3a114271867
Jonathan Caillaux <jonathan.caillaux@protonmail.com>
2025-01-25 22:28:54 +0100

    CHANGE Table

--- import_data.py
+++ import_data.py
@@ -3,14 +3,15 @@ import pandas as pd
 from utils.db_connection import build_engine
 from utils.table import table_dtype_mapping, table_name, table_data, prepare_df
 from utils.timer import Timer
-from tables.models import Base, schema, title_akas
+from tables.models import Base, schema
+from tables.models import title_akas, title_basics, title_crew, title_episode, title_principals, title_ratings, name_basics
+
+data_folder = './data/'
+models = [title_episode]#title_ratings, title_episode, title_crew, title_basics, name_basics, title_akas, title_principals]
 
 
 engine = build_engine()
-Base.metadata.create_all(engine, checkfirst=True)
-
-data_folder = './data/'
-models = [title_akas]
+Base.metadata.create_all(engine, checkfirst=True),
 
 chunksize = 500_000
 
