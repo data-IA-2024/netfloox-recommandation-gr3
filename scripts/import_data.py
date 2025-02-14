cd0d3ad8b1cdace6ae3e9f10e2aa43af1a61bea2
Jonathan Caillaux <jonathan.caillaux@protonmail.com>
2025-02-14 10:15:16 +0100

    ADD scripts folder

---
+++ scripts/import_data.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+from utils.db_connection import build_engine
+from utils.table import table_dtype_mapping, table_name, table_data, prepare_df, load_tsv
+from utils.timer import Timer
+from tables.models import Base, schema
+from tables.models import title_akas, title_basics, title_crew, title_episode, title_principals, title_ratings, name_basics
+
+data_folder = './data/'
+models = []#title_ratings, title_episode, title_crew, title_basics, name_basics, title_akas, title_principals]
+
+
+engine = build_engine()
+Base.metadata.create_all(engine, checkfirst=True),
+
+chunksize = 500_000
+
+with engine.connect() as conn:
+    timer = Timer(now=True)
+    for model in models:
+        filename = table_data(model)
+        name     = table_name(model)
+        dtype    = table_dtype_mapping(model)
+
+        print(filename)
+        
+        nb_rows_sent = 0
+
+        for chunk in load_tsv(data_folder + filename, chunksize=chunksize, nullchar=r'\N'):
+            chunk = prepare_df(chunk, dtype)
+            chunk.to_sql(name, con=engine, schema=schema, if_exists='append', index=False)
+            nb_rows_sent += chunk.shape[0]
+
+            print(f"\r{nb_rows_sent:_} rows uploaded. Elapsed Time : {timer.elapsed()}\033[0K", end="")
+        print()
